b61c9013eab78c991e9450cbafba71df0ae2f032
Jonas <jonasmo@gmail.com>
2018-03-08 23:26:18 +0100

    Completed for loops to create not gates and connections between them

--- ringosci.cpp
+++ ringosci.cpp
@@ -1,36 +1,51 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
+#include <cmath>
 
 using namespace std;
 
 int main()
 {
     int circuitStyleInput;
-    int numNotGates;
+    unsigned int numNotGates;
     string circuitStyle;
+    vector<int> notGates;
+
     cout << "Enter the circuit style you want (#):\n"
             "1. European\n"
-            "2. American\n" << endl;
+            "2. American" << endl;
     cin >> circuitStyleInput;
-    
-    if (circuitStyleInput == 1 || circuitStyleInput == 2) {
-        if (circuitStyleInput == 1) {
-            circuitStyle = "european";
-        } else {
-            circuitStyle = "american";
-        }
-    } else {
-        while(circuitStyleInput != 1 || circuitStyleInput ==2) {
+    while (circuitStyleInput != 1 && circuitStyleInput != 2) {
         cout << "Enter a valid circuit style" << endl;
         cin >> circuitStyleInput;
-        }
     }
 
+    if (circuitStyleInput == 1) {
+        circuitStyle = "european";
+    } else {
+        circuitStyle = "american";
+    }
+        
     cout << "Enter the number of NOT gates (odd number and more than 1):" << endl;
     cin >> numNotGates;
 
-    if (numNotGates > 1 && numNotGates % 2 !=0) {
+    while (numNotGates <= 1 || numNotGates % 2 == 0) { 
+        cout << "Enter a valid number of NOT gates" << endl;
+        cin >> numNotGates;
+    }
+
+    for (unsigned int i = 0; i != numNotGates; ++i) {
+        notGates.push_back(i); 
+    }
+
+    int numTopBottom = numNotGates/(log(numNotGates + 1));
+    int numRight = (numNotGates - numTopBottom*2)/2;
+    int numLeft = numRight + 1;
+    cout << numTopBottom << endl;
+    cout << numRight << endl;
+
     ofstream myfile;
     myfile.open ("ringosci.tex");
     myfile << "\\documentclass{article}\n"
@@ -39,20 +54,31 @@ int main()
               "\\begin{document}\n\n"
               "\\begin{figure}\n"
               "\\centering\n\n"
-              "\\begin{circuitikz} \\draw\n"
-              "(0,0) node[american not port] (1) {}\n"
-              "(3,0) node[american not port] (2) {}\n"
-              "(1.out) to (2.in);\n"
+              "\\begin{circuitikz} \\draw\n";
+
+    for (unsigned int i =1; i <= numTopBottom; ++i) {
+        myfile << "("<< 2*i <<",0) node[american not port] ("<< i <<") {}\n";
+    }
+
+    for (int i =1; i <= numRight; ++i) {
+        myfile << "("<< 2*(numTopBottom) <<","<< -3*(i) <<") node[american not port] ("<< numTopBottom + i <<") {}\n";
+    }
+
+    for (int i = numTopBottom, z =1; i >= 1, z <= numTopBottom; --i,++z) {
+        myfile << "("<< (2*i) << "," << -4*numRight << ") node[rotate=180, american not port] ("<< numTopBottom+numRight+z <<") {}\n";
+    }
+
+    for (int i = numLeft, z =1; i>=1,z <= numLeft; --i,++z) {
+        myfile << "("<< 2 <<"," << -2*i <<") node[rotate=180, american not port] ("<< numTopBottom+numRight+numTopBottom+z <<") {}\n";
+    }
+
+    for (int i = 1; i != numNotGates; ++i) {
+        myfile << "("<< i <<".out) -- (" << i+1 <<".in)";
+    }
+    myfile <<
               "\\end{circuitikz}\n"
               "\\end{figure}\n"
-              "\\end{document}";
+              "\\end{document}\n";
     myfile.close();
-    return 0;
 
-    } else {
-        while (numNotGates <= 1 || numNotGates % 2 == 0) {
-        cout << "Please enter a valid number of NOT gates!\n" << endl;
-        cin >> numNotGates;
-        }
-    }
 }
